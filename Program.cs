d5f882d888b50a56ac7f566a1c7c834aa80f4e7c
Engin Polat <enpolat@microsoft.com>
2025-04-11 13:54:10 -0700

    add JSON serialization context and configure HTTP JSON options for ToDo list handling

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+  options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
 
 builder.Services.AddValidation();
 
@@ -6,4 +13,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/get-todo-list", async context => await new GetToDoListHandler().Handle(context));
+
 app.Run();
+
+[JsonSerializable(typeof(GetToDoListRequestModel))]
+[JsonSerializable(typeof(GetToDoListResponseModel))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+}
